f59d38067237b7e218c4f1bae906844089d0b26d
Cory Bretsch <corybretsch@gmail.com>
2024-08-27 20:56:19 -0500

    completed user story requirements, adding error_serializer, poro error_message

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -51,13 +51,63 @@ describe "markets API" do
   end
 
   describe "show" do 
-    xit "can send one market" do 
-      # ...
+    describe "happy path" do
+      it "can send one market" do 
+        vendor1 = create(:vendor)
+        vendor2 = create(:vendor)
+        vendor3 = create(:vendor)
+        vendor4 = create(:vendor)
+        vendor5 = create(:vendor)
+        
+        market1 = create(:market)
+        market2 = create(:market)
+        market3 = create(:market)
+
+        MarketVendor.create!(market: market1, vendor: vendor1)
+        MarketVendor.create!(market: market1, vendor: vendor2)
+        MarketVendor.create!(market: market2, vendor: vendor3)
+        MarketVendor.create!(market: market2, vendor: vendor4)
+        MarketVendor.create!(market: market3, vendor: vendor5)
+
+        get api_v0_market_path(market1)
+
+        expect(response).to be_successful
+
+        market = JSON.parse(response.body, symbolize_names: true)[:data]
+      
+        expect(market).to have_key(:id)
+        expect(market[:id]).to be_a(String)
+
+        expect(market).to have_key(:type)
+        expect(market[:type]).to be_a(String)
+
+        expect(market).to have_key(:attributes)
+        expect(market[:attributes]).to be_a(Hash)
+
+        expect(market[:attributes][:name]).to eq(market1.name)
+        expect(market[:attributes][:street]).to eq(market1.street)
+        expect(market[:attributes][:city]).to eq(market1.city)
+        expect(market[:attributes][:county]).to eq(market1.county)
+        expect(market[:attributes][:state]).to eq(market1.state)
+        expect(market[:attributes][:zip]).to eq(market1.zip)
+        expect(market[:attributes][:lat]).to eq(market1.lat)
+        expect(market[:attributes][:lon]).to eq(market1.lon)
+        expect(market[:attributes][:vendor_count]).to eq(market1.count_vendors)
+      end
+    end
+
+    describe "sad path" do 
+      it "can return exception" do 
+        get "/api/v0/markets/123123123123"
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(404)
+
+        data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(data[:errors]).to be_a(Array)
+        expect(data[:errors].first[:detail]).to eq("Couldn't find Market with 'id'=123123123123")
+      end
     end
   end
-  
 end
-
-
-# This endpoint should follow the pattern of GET /api/v0/markets and should return ALL markets in the database.
-# In addition to the market’s main attributes, the market resource should also list an attribute for vendor_count, which is the number of vendors that are associated with that market.
