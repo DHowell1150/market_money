5ed18ee0af7ff2338d1beafe578022611d9d8616
Cory Bretsch <corybretsch@gmail.com>
2024-08-28 12:40:08 -0500

    added index to vendor controller, routes, and happy/sad path to vendor_spec

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -1,4 +1,15 @@
 class Api::V0::VendorsController < ApplicationController
+
+  def index 
+    begin
+      market = Market.find(params[:market_id])
+      render json: VendorSerializer.new(market.vendors)
+    rescue ActiveRecord::RecordNotFound => exception
+      render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
+      .serialize_json, status: :not_found
+    end
+  end
+
   def show
     begin
       render json: VendorSerializer.new(Vendor.find(params[:id]))
