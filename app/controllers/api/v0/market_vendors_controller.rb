19348cf47eea8827059245f0ac2b1e8e8c15d279
Cory Bretsch <corybretsch@gmail.com>
2024-08-29 22:20:55 -0500

    user story 8 happy path, sad path 1, and sad path 2 test

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -1,9 +1,9 @@
 class Api::V0::MarketVendorsController < ApplicationController 
 
   def create
-    if market = Market.find(params[:market_vendor_params][:market_id]) && vendor = Vendor.find(params[:market_vendor_params][:vendor_id])
-    
     begin
+      market = Market.find(params[:market_vendor_params][:market_id])
+      vendor = Vendor.find(params[:market_vendor_params][:vendor_id])
       MarketVendor.create(market: market, vendor: vendor)
       render json: MarketVendorSerializer.new.message, status: 201
     rescue ActiveRecord::RecordNotFound => exception
@@ -11,7 +11,7 @@ class Api::V0::MarketVendorsController < ApplicationController
     end
   end
 end
-end
+
 
 
 
