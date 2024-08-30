f5c00a59f910e61f63fb1fb94d7c7f06444cd430
Dana Howell <howelld115@gmail.com>
2024-08-30 09:57:55 -0600

    deletes commented out sections in controllers

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -1,6 +1,4 @@
 class Api::V0::VendorsController < ApplicationController
-  # rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
-
   def index 
       market = Market.find(params[:market_id])
       render json: VendorSerializer.new(market.vendors)
@@ -32,11 +30,6 @@ def destroy
 end
 
   private
-
-  # def not_found_response(exception)
-  #   render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
-  #     .serialize_json, status: :not_found
-  # end
 
   def vendor_params
     params.require(:vendor).permit(:name, :description, :contact_name, :contact_phone, :credit_accepted)
